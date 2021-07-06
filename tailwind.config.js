0ea786e2e526db3d099df45af2ddaf4342caaa29
Adrián Juárez <comecacahuates@yahoo.com>
2021-07-06 01:45:57 -0500

    Agregar estilos al tema de Tailwind CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,44 @@
 module.exports = {
-  mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  mode: "jit",
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ["Fira Sans", "sans-serif"],
+      display: ["Noto Serif JP", "serif"],
+    },
+    extend: {
+      transitionProperty: {
+        height: "height",
+      },
+      spacing: {
+        // vw units
+        "10vw": "10vw",
+        "20vw": "20vw",
+        "30vw": "30vw",
+        "40vw": "40vw",
+        "50vw": "50vw",
+        "60vw": "60vw",
+        "70vw": "70vw",
+        "80vw": "80vw",
+        "90vw": "90vw",
+        "100vw": "100vw",
+        // vh units
+        "10vh": "10vh",
+        "20vh": "20vh",
+        "30vh": "30vh",
+        "40vh": "40vh",
+        "50vh": "50vh",
+        "60vh": "60vh",
+        "70vh": "70vh",
+        "80vh": "80vh",
+        "90vh": "90vh",
+        "100vh": "100vh",
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
