dfb6d297c0e3062f7bdafd8b9e1d93e94e0760f6
Adrián Juárez <comecacahuates@yahoo.com>
2021-07-17 03:47:21 -0500

    Hero responsivo

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,8 @@ import * as React from "react";
 import Link from "next/link";
 import Image from "next/image";
 import Layout from "../components/layout";
+import SectionTitle from "../components/sectiontitle";
+import SectionImg from "../components/sectionimg";
 import Icon from "../components/icon";
 
 export default function Home() {
@@ -10,22 +12,19 @@ export default function Home() {
       route="/"
       title="Servicios de Ingeniería CAD/CAM | Inicio"
       author="Adrián Juárez Monroy"
-      description="Soluciones para tus necesidades de fabricación digital"
+      description="Las mejor fabricación digital"
     >
       {/* Título */}
-      <section className="pt-5 h-screen flex flex-col">
-        <div className="flex-1 flex items-center mx-auto px-3">
-          <div>
-            <p className="h3 text-blue-dark text-center">
-              Servicios de ingeniería CAD/CAM
-            </p>
-            <h1 className="text-center">
+      <section className="h-screen flex flex-col lg:flex-row">
+        <div className="flex-none lg:flex-1 lg:flex items-center mx-auto px-3 lg:px-8">
+          <div className="py-12 text-center lg:text-right">
+            <h1 className="text-my-blue-dark">
               Las mejores herramientas para fabricación digital
             </h1>
-            <p className="h3 text-center">Diseño 3D & Fabricación</p>
+            <p className="h4 text-gray-500">Diseño 3D & Fabricación</p>
           </div>
         </div>
-        <div className="flex-1 h-96 relative">
+        <div className="flex-1 relative">
           <Image
             src="https://placeimg.com/640/480/nature"
             layout="fill"
@@ -35,178 +34,81 @@ export default function Home() {
         </div>
       </section>
       {/* Diseño 3D */}
-      <section className="flex flex-col">
-        <div className="h-80 flex items-center bg-my-blue text-white">
-          <div className="flex-1">
-            <h2 className="text-center">Diseño 3D</h2>
-          </div>
-        </div>
+      <section className="bg-my-blue text-white flex flex-col">
+        <SectionTitle textColor="white">Diseño 3D</SectionTitle>
         {/* Rhinoceros */}
-        <div className="container flex flex-col">
-          <h1>Rhino</h1>
-        </div>
-      </section>
-      {/* Creación del modelo 3D */}
-      <section className="px-3 pt-10 bg-my-yellow">
-        <div className="container mx-auto">
-          <div className="flex flex-col">
-            <div>
-              <h2 className="text-black">Creación de modelos 3D</h2>
-              <p className="lead">
-                Genera la geometría de tus modelos desde cero o con un modelo
-                físico de referencia.
-              </p>
-            </div>
-            <div className="flex flex-col py-3">
-              {/* Diseño 3D */}
-              <div className="h-60">
-                <h3 className="text-black">Diseño 3D</h3>
-                <p>
-                  Crea en la computadora el modelo que imaginaste con las
-                  herramientas que te ofrece Rhinoceros.
-                </p>
-                <p>
-                  <Link href="#">
-                    <a className="text-black">
-                      Más información <Icon icon="arrow-right" />
-                    </a>
-                  </Link>
-                </p>
-              </div>
-              {/* Ingeniería inversa */}
-              <div className="h-60">
-                <h3 className="text-black">Ingeniería inversa</h3>
-                <p>
-                  Lleva un modelo físico al mundo digital mediante los brazos
-                  digitalizadores MicroScribe.
-                </p>
-                <p>
-                  <Link href="#">
-                    <a className="text-black">
-                      Más información <Icon icon="arrow-right" />
-                    </a>
-                  </Link>
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
-      {/* Imagen */}
-      <div className="h-96 relative diagonal-top-right before:bg-my-yellow">
-        <Image
-          src="https://placeimg.com/640/480/nature"
-          layout="fill"
-          objectFit="cover"
-          objectPosition="50% 50%"
-        />
-      </div>
-      {/* Métodos de fabricación */}
-      <section className="px-3 py-5">
-        <div className="container mx-auto">
-          <div className="flex flex-col">
-            <div>
-              <h2>Métodos de fabricación</h2>
-              <p className="lead">
-                Planifica las herramientas con las que fabricarás tu idea.
-              </p>
-            </div>
-            <div className="flex flex-col py-3">
-              {/* Maquinado CNC */}
-              <div className="h-60">
-                <h3>Maquinado CNC</h3>
-                <p>
-                  Genera los códigos de maquinado para fabricación CNC con
-                  RhinoCAM.
-                </p>
-                <p>
-                  <Link href="#">
-                    <a>
-                      Más información <Icon icon="arrow-right" />
-                    </a>
-                  </Link>
-                </p>
-              </div>
-              {/* Construcción por rebanadas */}
-              <div className="h-60">
-                <h3>Construcción por rebanadas</h3>
-                <p>
-                  Utiliza Grasshopper para crear rebanadas a partir de tu modelo
-                  y prepáralas para corte.
-                </p>
-                <p>
-                  <Link href="#">
-                    <a>
-                      Más información <Icon icon="arrow-right" />
-                    </a>
-                  </Link>
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
+        <SectionImg src="https://placeimg.com/640/480/nature" alt="Alt">
+          <h3>Rhinoceros</h3>
+          <p className="lead">
+            La navaja suiza del diseño 3D. Crea, edita, analiza y anima modelos
+            fácil.
+          </p>
+          <p>
+            <Link href="#">
+              <a>
+                Más información <Icon icon="arrow-right" />
+              </a>
+            </Link>
+          </p>
+        </SectionImg>
+        {/* Grasshopper */}
+        <SectionImg src="https://placeimg.com/640/480/nature" alt="Alt">
+          <h3>Grasshopper</h3>
+          <p className="lead">
+            Un editor gráfico de algoritmos con el que podrás generar geometría
+            compleja con muy poco esfuerzo.
+          </p>
+        </SectionImg>
+        {/*     Digitalizadores MicroScribe */}
+        <SectionImg src="https://placeimg.com/640/480/nature" alt="Alt">
+          <h3>Digitalizadores MicroScribe</h3>
+          <p className="lead">
+            Utiliza modelos reales como referencia para recrearlos digitalmente.
+          </p>
+        </SectionImg>
       </section>
       {/* Fabricación */}
-      <section className="px-3 py-5">
-        <div className="container mx-auto">
-          <div className="flex flex-col">
-            <div>
-              <h2>Fabricación</h2>
-              <p className="lead">
-                Utiliza la tecnología para llevar tu modelo al mundo real
-              </p>
-            </div>
-            <div className="flex flex-col py-3">
-              {/* Maquinado CNC */}
-              <div className="h-60">
-                <h3>Maquinado CNC</h3>
-                <p>
-                  Utiliza tus códigos de maquinado para fabricar tu modelo con
-                  una máquina CNC.
-                </p>
-                <p>
-                  <Link href="#">
-                    <a>
-                      Más información <Icon icon="arrow-right" />
-                    </a>
-                  </Link>
-                </p>
-              </div>
-              {/* Impresión 3D */}
-              <div className="h-60">
-                <h3>Impresión 3D</h3>
-                <p>Obtén prototipos físicos muy fácil y rápidamente.</p>
-                <p>
-                  <Link href="#">
-                    <a>
-                      Más información <Icon icon="arrow-right" />
-                    </a>
-                  </Link>
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
+      <section className="flex flex-col">
+        <SectionTitle textColor="black">Fabricación</SectionTitle>
+        {/* RhinoCAM */}
+        <SectionImg src="https://placeimg.com/640/480/nature" alt="Alt">
+          <h3>RhinoCAM</h3>
+          <p className="lead">
+            Genera los códigos de maquinado CNC dentro de Rhinoceros para
+            fabricar tus modelos.
+          </p>
+        </SectionImg>
+        {/* Máquinas CNC */}
+        <SectionImg src="https://placeimg.com/640/480/nature" alt="Alt">
+          <h3>Máquinas CNC</h3>
+          <p className="lead">
+            Fabrica tus modelos en una gran cantidad de materiales.
+          </p>
+        </SectionImg>
+        {/* Impresión Impresoras 3D */}
+        <SectionImg src="https://placeimg.com/640/480/nature" alt="Alt">
+          <h3>Impresoras 3D</h3>
+          <p className="lead">
+            Obtén prototipos y piezas funcionales en cuestión de horas.
+          </p>
+        </SectionImg>
       </section>
       {/* Educación */}
-      <section className="px-3 py-5">
-        <div className="container mx-auto">
-          <div className="flex flex-col">
-            <div>
-              <h2>Educación</h2>
-              <p className="lead">
-                Capacítate con nosotros para dominar las tecnologías y
-                herramientas que ponemos a tu disposición.
-              </p>
-              <p>
-                <Link href="#">
-                  <a>
-                    Más información <Icon icon="arrow-right" />
-                  </a>
-                </Link>
-              </p>
-            </div>
-          </div>
+      <section className="flex flex-col bg-my-yellow">
+        <SectionTitle textColor="black">Educación</SectionTitle>
+        <div className="container mx-auto px-3 py-10">
+          <p className="lead">
+            Aprende con nosotros a utilizar las tecnologías que ponemos a tu
+            disposición.
+          </p>
+        </div>
+        <div className="h-96 relative">
+          <Image
+            src="https://placeimg.com/640/480/nature"
+            layout="fill"
+            objectFit="cover"
+            objectPosition="50% 50%"
+          />
         </div>
       </section>
     </Layout>
