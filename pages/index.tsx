8e8ad86c18906b5d70e7f66f662a969b745b7ec2
Adrián Juárez <comecacahuates@yahoo.com>
2021-07-17 06:53:42 -0500

    Sección de fabricación responsiva

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,14 +15,14 @@ export default function Home() {
       description="Las mejor fabricación digital"
     >
       {/* Hero */}
-      <section className="h-screen flex flex-col lg:grid lg:grid-cols-2">
+      <section className="mb-36 h-screen flex flex-col lg:grid lg:grid-cols-2">
         {/* Título */}
         <div className="flex-none lg:flex items-center mx-auto px-3 lg:px-8">
           <div className="py-12 text-center lg:text-right">
             <h1 className="text-my-blue-dark">
               Las mejores herramientas para fabricación digital
             </h1>
-            <p className="h4 text-gray-500">Diseño 3D & Fabricación</p>
+            <p className="h3 text-gray-500">Diseño 3D / Fabricación</p>
           </div>
         </div>
         {/* Imagen */}
@@ -36,13 +36,13 @@ export default function Home() {
         </div>
       </section>
       {/* Diseño 3D */}
-      <section className="my-36 md:h-screen md:grid md:grid-cols-2 xl:container xl:mx-auto">
+      <section className="mb-36 md:h-screen md:grid md:grid-cols-2 md:auto-rows-fr xl:container xl:mx-auto">
         <div className="h-40 md:h-auto md:flex md:items-center md:justify-center">
           <h2 className="text-center">Diseño 3D</h2>
         </div>
         {/* Rhinoceros */}
         <div className="h-96 md:h-auto relative md:order-first">
-          <div className="absolute inset-0 z-10 px-3 md:px-10 py-12 flex items-center justify-center xl:px-10">
+          <div className="absolute inset-0 z-10 px-3 md:px-10 flex items-center justify-center">
             <div className="text-center">
               <h3>
                 <Link href="#">
@@ -66,7 +66,7 @@ export default function Home() {
         </div>
         {/* Grasshopper */}
         <div className="h-96 md:h-auto relative">
-          <div className="absolute inset-0 z-10 px-3 md:px-10 py-12 flex items-center justify-center xl:px-10">
+          <div className="absolute inset-0 z-10 px-3 md:px-10 flex items-center justify-center xl:px-10">
             <div className="text-center">
               <h3>
                 <Link href="#">
@@ -90,7 +90,7 @@ export default function Home() {
         </div>
         {/* Digitalizadores MicroScribe */}
         <div className="h-96 md:h-auto relative">
-          <div className="absolute inset-0 z-10 px-3 md:px-10 py-12 flex items-center justify-center xl:px-10">
+          <div className="absolute inset-0 z-10 px-3 md:px-10 flex items-center justify-center xl:px-10">
             <div className="text-center">
               <h3>
                 <Link href="#">
@@ -115,30 +115,83 @@ export default function Home() {
         </div>
       </section>
       {/* Fabricación */}
-      <section className="flex flex-col">
-        <SectionTitle textColor="black">Fabricación</SectionTitle>
+      <section className="mb-36 md:h-screen md:grid md:grid-cols-2 md:auto-rows-fr xl:container xl:mx-auto">
+        <div className="h-40 md:h-auto md:flex md:items-center md:justify-center">
+          <h2 className="text-center">Fabricación</h2>
+        </div>
         {/* RhinoCAM */}
-        <SectionImg src="https://placeimg.com/640/480/nature" alt="Alt">
-          <h3>RhinoCAM</h3>
-          <p className="lead">
-            Genera los códigos de maquinado CNC dentro de Rhinoceros para
-            fabricar tus modelos.
-          </p>
-        </SectionImg>
+        <div className="h-96 md:h-auto relative">
+          <div className="absolute inset-0 z-10 px-3 md:px-10 flex items-center justify-center">
+            <div className="text-center">
+              <h3>
+                <Link href="#">
+                  <a className="text-white">RhinoCAM</a>
+                </Link>
+              </h3>
+              <p className="lead text-white">
+                Genera los códigos de maquinado CNC dentro de Rhinoceros para
+                fabricar tus modelos.
+              </p>
+            </div>
+          </div>
+          <div className="absolute inset-0 filter brightness-40">
+            <Image
+              src="https://placeimg.com/640/480/nature"
+              alt="RhinoCAM: software de maquinado CNC"
+              layout="fill"
+              objectFit="cover"
+              objectPosition="center center"
+            />
+          </div>
+        </div>
         {/* Máquinas CNC */}
-        <SectionImg src="https://placeimg.com/640/480/nature" alt="Alt">
-          <h3>Máquinas CNC</h3>
-          <p className="lead">
-            Fabrica tus modelos en una gran cantidad de materiales.
-          </p>
-        </SectionImg>
-        {/* Impresión Impresoras 3D */}
-        <SectionImg src="https://placeimg.com/640/480/nature" alt="Alt">
-          <h3>Impresoras 3D</h3>
-          <p className="lead">
-            Obtén prototipos y piezas funcionales en cuestión de horas.
-          </p>
-        </SectionImg>
+        <div className="h-96 md:h-auto relative">
+          <div className="absolute inset-0 z-10 px-3 md:px-10 flex items-center justify-center">
+            <div className="text-center">
+              <h3>
+                <Link href="#">
+                  <a className="text-white">Máquinas CNC</a>
+                </Link>
+              </h3>
+              <p className="lead text-white">
+                Fabrica tus modelos en una gran cantidad de materiales.
+              </p>
+            </div>
+          </div>
+          <div className="absolute inset-0 filter brightness-40">
+            <Image
+              src="https://placeimg.com/640/480/nature"
+              alt="Máquina CNC"
+              layout="fill"
+              objectFit="cover"
+              objectPosition="center center"
+            />
+          </div>
+        </div>
+        {/* Impresoras 3D */}
+        <div className="h-96 md:h-auto relative">
+          <div className="absolute inset-0 z-10 px-3 md:px-10 flex items-center justify-center">
+            <div className="text-center">
+              <h3>
+                <Link href="#">
+                  <a className="text-white">Impresoras 3D</a>
+                </Link>
+              </h3>
+              <p className="lead text-white">
+                Obtén prototipos y piezas funcionales en cuestión de horas.
+              </p>
+            </div>
+          </div>
+          <div className="absolute inset-0 filter brightness-40">
+            <Image
+              src="https://placeimg.com/640/480/nature"
+              alt="Impresora 3D"
+              layout="fill"
+              objectFit="cover"
+              objectPosition="center center"
+            />
+          </div>
+        </div>
       </section>
       {/* Educación */}
       <section className="flex flex-col bg-my-yellow">
